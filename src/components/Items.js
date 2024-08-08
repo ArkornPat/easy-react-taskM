38725359bc2ce8fa2906c2a5f7ac26164ada7cfc
unknown <arkorn.pat@gmail.com>
2024-08-08 16:44:17 +0700

    edit Items.js

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import { FaRegTrashCan } from "react-icons/fa6";
 import { FaRegEdit } from "react-icons/fa";
 import "./Items.css"
 
 export default function Items(props) {
   const{data,deleteTask, editTask} = props;
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
     <div className="itens-list">
-      <p className="items-title">{data.title}</p>
+      <input
+        type="checkbox"
+        onChange={handleCheckboxChange}
+        className="checkbox"
+      />
+      <p className={`items-title ${isChecked ? 'checked' : ''}`}>{data.title}</p>
       <div className="btn-container">
         <FaRegEdit className="btn"onClick={()=>editTask(data.id)}/>
         <FaRegTrashCan className="btn" onClick={()=>deleteTask(data.id)}/>
